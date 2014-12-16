c6c075dabd6f725606a9527a57915d994b8ec89c
Ranjith <ranjith.kumar@zippr.in>
2014-12-16 11:25:59 +0530

    Initial Commit

---
+++ Contacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Contacts
+//
+//  Created by Ranjith on 16/12/14.
+//  Copyright (c) 2014 Zippr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
